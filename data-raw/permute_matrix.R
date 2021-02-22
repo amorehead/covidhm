da0e5f1089de9b2e98d91e7a43e3a04b399dde10
Alex Morehead <alex.morehead@gmail.com>
2021-02-22 13:40:27 -0600

    Add logic to permute full matrix of AM lists

--- data-raw/permute_matrix.R
+++ data-raw/permute_matrix.R
@@ -1,9 +1,20 @@
 # Load original AM dataframes from a single file
-load("data-raw/am_7 and am_16.RData")
+load("data-raw/am_list_original.RData")
+# load("data-raw/am_7 and am_16_original.RData")
 
 # Permute AM dataframes row-wise
-am_7_permuted <- am_7[sample(nrow(am_7)),]
-am_16_permuted <- am_16[sample(nrow(am_16)),]
+# AM shuffling
+All <- am_list[[1]][sample(nrow(am_list[[1]])),]
+Thurs <- am_list[[2]][sample(nrow(am_list[[2]])),]
+Fri <- am_list[[3]][sample(nrow(am_list[[3]])),]
+Sat <- am_list[[4]][sample(nrow(am_list[[4]])),]
+am_list = list(All, Thurs, Fri, Sat)
+names(am_list) = c("All", "Thurs", "Fri", "Sat")
+
+# AM 7-16 shuffling
+# am_7 <- am_7[sample(nrow(am_7)),]
+# am_16 <- am_16[sample(nrow(am_16)),]
 
 # Save permuted AM dataframes in a single file
-save(am_7_permuted, am_16_permuted, file="data-raw/am_7_and_am_16_permuted.RData")
+save(am_list, file="data-raw/am_list.RData")
+# save(am_7, am_16, file="data-raw/am_7 and am_16.RData")
